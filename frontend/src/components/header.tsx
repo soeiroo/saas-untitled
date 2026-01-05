3c8d518404b54fcaff52063baf49a98a68244f0c
Ryenan <ryenanaragaorodrigues@gmail.com>
2026-01-05 18:27:53 -0300

    Header atualizado

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -5,10 +5,10 @@ const Header = () => {
         <div className="w-full bg-neutral-800 flex flex-row items-center justify-between px-16">
             <h1 className="h-24 text-4xl flex items-center font-bold">Saas Untlited</h1>
             <li className="flex flex-row gap-10">
-               <Link href="/">Home</Link>
-               <Link href="/">Pag 2</Link>
-               <Link href="/">Pag 3</Link>
-               <Link href="/">Pag 4</Link>
+                <Link href="/" className="transition-transform duration-200 hover:scale-110 hover:text-purple-50">Home</Link>
+                <Link href="/" className="transition-transform duration-200 hover:scale-110 hover:text-purple-500">Pag 2</Link>
+                <Link href="/" className="transition-transform duration-200 hover:scale-110 hover:text-purple-500">Pag 3</Link>
+                <Link href="/" className="transition-transform duration-200 hover:scale-110 hover:text-purple-500">Pag 4</Link>
             </li>
         </div>
     );
