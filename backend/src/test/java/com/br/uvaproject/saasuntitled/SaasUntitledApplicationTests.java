34adf0b24f0fa48d15ef3274f79f216401c0040c
JonasCandido <jonasdcandido210@gmail.com>
2026-01-06 09:53:55 -0300

    Add folder structure backend

---
+++ backend/src/test/java/com/br/uvaproject/saasuntitled/SaasUntitledApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.uvaproject.saasuntitled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasUntitledApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
