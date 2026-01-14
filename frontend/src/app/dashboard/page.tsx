9e1720ccaee622e0efaf93746723125de6591f9f
soeiroo <friskylt8@gmail.com>
2026-01-14 20:42:00 -0300

    hot-fix/token-verifier in dashboard /

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,5 +1,19 @@
 import HomePage from '@/pages/HomePage';
+import { useRouter } from 'next/dist/client/components/navigation';
+import { useEffect } from 'react';
+
 
 export default function Page() {
+  const router = useRouter();
+  
+  useEffect(() => {
+      // verificar se o usuário está autenticado
+      const token = localStorage.getItem('authToken');
+      
+      if (!token) {
+        router.replace('/login');
+      }
+  }, [router]);
+        
   return <HomePage />;
 }
