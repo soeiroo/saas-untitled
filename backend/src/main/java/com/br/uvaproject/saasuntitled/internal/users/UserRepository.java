9ae842ea79c8627ba3d6b0e6fd38f749e5b4fb2c
JonasCandido <jonasdcandido210@gmail.com>
2026-01-26 12:08:02 -0300

    Forgot to import list to UserRepository

--- backend/src/main/java/com/br/uvaproject/saasuntitled/internal/users/UserRepository.java
+++ backend/src/main/java/com/br/uvaproject/saasuntitled/internal/users/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
